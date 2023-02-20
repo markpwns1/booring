fbe0937894ec1b1af292578ca5aba723bd74c613
Mark <mark_kaldas@yahoo.com>
2023-02-20 12:55:25 -0700

    fixed env.port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const http = require('http');
 const axios = require('axios');
 const app = express();
-const port = env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const HTML_TEMPLATE = fs.readFileSync("embed.html", "utf-8");
 
