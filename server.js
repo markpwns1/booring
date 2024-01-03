6d16a459c1cf6a2757167e28888a1020fa1c133f
Mark <mark_kaldas@yahoo.com>
2024-01-02 23:01:51 -0700

    fixed autocomplete for certain sites

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ const PROXIES = {
     },
     "rule34": {
         "Referrer": "https://rule34.xxx/",
+        "Referer": "https://rule34.xxx/",
+        "Host": "rule34.xxx",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0"
     },
     "konachan": {
         "Referrer": "https://konachan.com/"
